108c223c1181f6fe9a93676a328ced22dacecc2c
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-06-17 11:52:22 +0300

    Delete AppBundle and fix problems with autoload

--- src/RestBundle/Controller/DefaultController.php
+++ src/RestBundle/Controller/DefaultController.php
@@ -8,6 +8,6 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('RestBundle:Default:index.html.twig');
+        return $this->render('@Rest/Default/index.html.twig');
     }
 }
