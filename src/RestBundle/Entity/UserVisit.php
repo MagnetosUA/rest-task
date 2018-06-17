a59f59ff2829afb48cc0c2dee437c71e39ccb37a
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-06-17 21:41:26 +0300

    Create UserVisit entity for registering date of user's visit

---
+++ src/RestBundle/Entity/UserVisit.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace RestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * UserVisit
+ *
+ * @ORM\Table(name="user_visit")
+ * @ORM\Entity(repositoryClass="RestBundle\Repository\UserVisitRepository")
+ */
+class UserVisit
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="userId", type="integer")
+     */
+    private $userId;
+
+    /**
+     * @var \Date
+     *
+     * @ORM\Column(name="visitDate", type="date")
+     */
+    private $visitDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set userId
+     *
+     * @param integer $userId
+     *
+     * @return UserVisit
+     */
+    public function setUserId($userId)
+    {
+        $this->userId = $userId;
+
+        return $this;
+    }
+
+    /**
+     * Get userId
+     *
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->userId;
+    }
+
+    /**
+     * Set visitDate
+     *
+     * @param \DateTime $visitDate
+     *
+     * @return UserVisit
+     */
+    public function setVisitDate($visitDate)
+    {
+        $this->visitDate = $visitDate;
+
+        return $this;
+    }
+
+    /**
+     * Get visitDate
+     *
+     * @return \DateTime
+     */
+    public function getVisitDate()
+    {
+        return $this->visitDate;
+    }
+}
+
