6e2d99737b1fc98c77fb4fd165534664825bb15d
Aleks Sushitskiy <magnetos.ua@gmail.com>
2018-06-17 17:09:15 +0300

    Create Create ability of REST CRUD

--- src/RestBundle/Controller/UserController.php
+++ src/RestBundle/Controller/UserController.php
@@ -13,11 +13,17 @@ use RestBundle\Entity\User;
 
 class UserController extends FOSRestController
 {
-    public function indexAction()
-    {
-        return $this->render('@Rest/Default/index.html.twig');
-    }
+//    /**
+//     * @Rest\Get("/")
+//     */
+//    public function indexAction()
+//    {
+//        return $this->render('@Rest/Default/index.html.twig');
+//    }
 
+    /**
+     * @Rest\Get("/get-user")
+     */
     public function getAction()
     {
         $restresult = $this->getDoctrine()->getRepository('RestBundle:User')->findAll();
@@ -25,6 +31,27 @@ class UserController extends FOSRestController
             return new View("there are no users exist", Response::HTTP_NOT_FOUND);
         }
         return $restresult;
+    }
+
+    /**
+     * @Rest\Post("/post-user/")
+     */
+    public function postAction(Request $request)
+    {
+        $data = new User;
+        $name = $request->get('name');
+        $login = $request->get('login');
+        if(empty($name) || empty($login))
+        {
+            return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
+        }
+        $data->setName($name);
+        $data->setLogin($login);
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($data);
+        $em->flush();
+
+        return new View("User Added Successfully", Response::HTTP_OK);
     }
 
 }
